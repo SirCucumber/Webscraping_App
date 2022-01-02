8d1b92622b7e71c07982ccbd2fcaf3931a7d54fb
Green Room <isdustbin@gmail.com>
2022-01-02 21:18:53 +0300

    рабочая версия на puppeteer, бeз рассылки

--- index.js
+++ index.js
@@ -1,52 +1,43 @@
 const puppeteer = require("puppeteer");
-const cheerio = require("cheerio");
 const CronJob = require("cron").CronJob;
 const nodemailer = require("nodemailer");
 const itemsList = require("./items.json");
 
-const headphone = itemsList["Sennheiser HD 280 Pro"];
-
-async function configireBrowser() {
+async function configireBrowser(url) {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto(headphone);
+  await page.goto(url);
   return page;
 }
 
-async function checkPrice(page) {
-  await page.reload();
-  let html = await page.evaluate(() => document.body.innerHTML);
-  const $ = cheerio.load(html);
-  const rublePrice = [];
+async function checkPrice(page, item, locator) {
+  let rublePrice = await page.$eval(locator, (el) => el.innerText);
+  let currentPrice = await Number(rublePrice.replace(/[^0-9.-]+/g, ""));
 
-  $(".ProductPrice__price.ProductHeader__price-default__price", html).each(
-    function () {
-      rublePrice.push($(this).text());
-
-      let currentPrice = Number(rublePrice[0].replace(/[^0-9.-]+/g, ""));
-
-      if (currentPrice < 7000) {
-        console.log("NEW PRICE! " + currentPrice);
-      }
-    }
-  );
+  if (currentPrice < item.price) {
+    console.log(`"Цена на ${item.name} упала до ${currentPrice}!`);
+  }
 }
 
 async function startTracking() {
-  const page = await configireBrowser();
+  for (let property in itemsList) {
+    itemsList[property].items.forEach(async (item) => {
+      const page = await configireBrowser(item.url);
 
-  let job = new CronJob(
-    "*/15 * * * * *",
-    function () {
-      checkPrice(page);
-    },
-    null,
-    true,
-    null,
-    null,
-    true
-  );
-  job.start();
+      let job = new CronJob(
+        "*/15 * * * * *",
+        function () {
+          checkPrice(page, item, itemsList[property].locator);
+        },
+        null,
+        true,
+        null,
+        null,
+        true
+      );
+      job.start();
+    });
+  }
 }
 
 startTracking();
